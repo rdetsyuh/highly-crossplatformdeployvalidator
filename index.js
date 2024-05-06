1b7e5fd4404e30de29e1af6b9c99c7783365df3b
rdetsyuh <gs1josefinu@hotmail.com>
2024-05-07 02:11:11 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-function subarraySum(nums, k) {
-  const map = new Map();
-  map.set(0, 1);
-  let count = 0;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (map.has(sum - k)) {
-      count += map.get(sum - k);
-    }
-    map.set(sum, (map.get(sum) || 0) + 1);
-  }
-  return count;
+const number = 10;
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
 }
