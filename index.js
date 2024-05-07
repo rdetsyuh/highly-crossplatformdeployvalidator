2158862d2cb5f725fcfeb3b246cc8160262ad1e1
rdetsyuh <gs1josefinu@hotmail.com>
2024-05-08 01:58:31 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const number = 10;
-if (number % 2 === 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
+  }
+  return minHeap.size();
 }
